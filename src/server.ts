016994967877920539c0c5b313fa775d2c9913f7
Mina S. Aziz <minaz922@gmail.com>
2018-04-02 21:13:32 +0200

    implement video streaming logic

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,6 @@
 import * as express from 'express';
+import * as path from 'path';
+import * as fs from 'fs';
 import * as winston from 'winston';
 import {json} from 'body-parser';
 import * as morgan from 'morgan';
@@ -45,9 +47,51 @@ export class Server {
    * it might be better to move this to a separate class.
    */
   private static setupRoutes(app: Express): void {
-    app.get('/', (req: Request, res: Response) => {
-      res.status(200).send('Server running ...');
+    app.get('/vid', (req: Request, res: Response) => {
+      // res.status(200).send('Server running ...');
+      const { filename } = req.query;
+      const file = path.resolve(VIDEOS_DIR, filename);
+      fs.stat(file, (err, stats) => {
+        if (err) {
+          if (err.code === 'ENOENT') {
+            // 404 Error if file not found
+            return res.sendStatus(404);
+          }
+          res.end(err);
+        }
+        const range = req.headers.range;
+        if (!range) {
+          // 416 wrong range
+          return res.sendStatus(416);
+        }
+        const positions: string[] = range.replace(/bytes=/, '').split('-');
+        const start = parseInt(positions[0], 10);
+        const total = stats.size;
+        let end = positions[1] ? parseInt(positions[1], 10) : total - 1;
+        let chunkSize =  (end - start ) + 1;
+        const maxChunk = 1024 * 1024; // 1 MB at a time
+
+        if (chunkSize > maxChunk) {
+          end = start + maxChunk - 1;
+          chunkSize = (end - start) + 1;
+        }
+
+        res.writeHead(206, {
+          'Content-Range': `bytes ${start}-${end}/${total}`,
+          'Accept-Ranges': 'bytes',
+          'Content-Length': chunkSize,
+          'Content-Type': 'video/mp4'
+        });
+
+        const stream = fs.createReadStream(file, { start, end })
+          .on('open', () => stream.pipe(res))
+          .on('error', (err) => res.end(err));
+      });
     });
+
+    app.get('/', (req: Request, res: Response) => {
+      res.sendFile(path.join(__dirname, 'index.html'));
+    })
   }
 }
 
